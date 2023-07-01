2f371dfb5fe52bfe87355886d9917aba2623a864
KThellez <Kthellez.ofi@gmail.com>
2023-06-30 22:33:54 -0500

    The initial configuration was done to use Jinja2 in conjunction with Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run
+data_home = {
+    "titulo_pagina": 'home',
+    "titulo_h1": 'Hola Mundo' 
+}
 
+@app.route('/')
+def index():
+    return render_template('index.html', home = data_home)
 
+@app.route('/login')
+def login():
+    pass
 
+@app.route('/register')
+def register():
+    pass
+
+@app.route('/Modelo1')
+def modelo_uno():
+    pass
+
+@app.route('/Modelo2')
+def modelo_dos():
+    pass
+
+@app.route('/Modelo3')
+def modelo_tres():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7777)
